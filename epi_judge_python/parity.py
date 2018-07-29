e99129c254b00ffadabd78ee9a2de67bb6f62350
ChengyuanCai <kevin.c.cai@gatech.edu>
2018-07-28 21:58:27 -0400

    Complete parity bits

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -12,24 +12,24 @@ from test_framework import generic_test
 #     return numOnes % 2
 
 #O(k), let k be the number of bits set to 1 in a particular word. O(1) Space
-# def parity(x):
-#     result = 0
-#     while x:
-#         result = result ^ 1
-#         x = x & (x - 1)
+def parity(x):
+    result = 0
+    while x:
+        result = result ^ 1
+        x = x & (x - 1)
 
 #O(n/L) time complexity with caching where n is the word size(For example 64 bits)
 #and L is the width of the key in bits (For example 16 bits)
 
 #O(log n) solution where n is the word size by using XOR. We can combine caching and this approach
-def parity(x):
-    x = x ^ (x >> 32)
-    x = x ^ (x >> 16)
-    x = x ^ (x >> 8)
-    x = x ^ (x >> 4)
-    x = x ^ (x >> 2)
-    x = x ^ (x >> 1)
-    return x & 0x1
+# def parity(x):
+#     x = x ^ (x >> 32)
+#     x = x ^ (x >> 16)
+#     x = x ^ (x >> 8)
+#     x = x ^ (x >> 4)
+#     x = x ^ (x >> 2)
+#     x = x ^ (x >> 1)
+#     return x & 0x1
 
 
 if __name__ == '__main__':
